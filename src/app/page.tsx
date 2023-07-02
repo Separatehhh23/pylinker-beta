3300c1fa7725af02b8a16d8db7ca365ac3597c55
Separatehhh23 <separatehhh23@gmail.com>
2023-07-02 22:22:54 +0200

    Removed firebse for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import NavBar, { DropdownMenu, NavItem } from 'c/NavBar';
+import Background from 'c/background';
 
 import { IoMdAdd, IoMdArrowDropdown, IoMdChatbubbles, IoMdNotificationsOutline } from 'react-icons/io';
 
 function App() {
     return (
-        <NavBar>
-            <NavItem icon={<IoMdNotificationsOutline />} />
-            <NavItem icon={<IoMdAdd />} />
-            <NavItem icon={<IoMdChatbubbles />} />
+        <div className='h-screen w-screen'>
+            <NavBar>
+                <NavItem icon={<IoMdNotificationsOutline />} />
+                <NavItem icon={<IoMdAdd />} />
+                <NavItem icon={<IoMdChatbubbles />} />
 
-            <NavItem icon={<IoMdArrowDropdown />}>
-                
-                <DropdownMenu />
+                <NavItem icon={<IoMdArrowDropdown />}>
+                    
+                    <DropdownMenu />
 
-            </NavItem>
-        </NavBar>
+                </NavItem>
+            </NavBar>
+
+            <Background />
+        </div>
     );
 }
 
